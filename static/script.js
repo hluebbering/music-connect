bf0d338eb6fab00270f324c2d9d48a8e385dcdbf
Hannah Luebbering <luebhr@gmail.com>
2024-09-07 13:49:35 -0400

    Prepare for deployment to GitHub Pages

--- static/script.js
+++ static/script.js
@@ -29,29 +29,3 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching data:', error));
 });
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('/get_data')
-        .then(response => response.json())
-        .then(data => {
-            const tracks = data.top_tracks.items;
-            const artists = data.top_artists.items;
-
-            const tracksList = document.getElementById('top-tracks');
-            tracks.forEach(track => {
-                const li = document.createElement('li');
-                li.textContent = `${track.name} by ${track.artists.map(artist => artist.name).join(', ')}`;
-                tracksList.appendChild(li);
-            });
-
-            const artistsList = document.getElementById('top-artists');
-            artists.forEach(artist => {
-                const li = document.createElement('li');
-                li.textContent = artist.name;
-                artistsList.appendChild(li);
-            });
-        })
-        .catch(error => console.error('Error fetching data:', error));
-});
